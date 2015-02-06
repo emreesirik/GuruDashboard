04c204f139c45809a19729a26d2b07f3868b06d7
emreesirik <emreesirik@gmail.com>
2015-02-06 19:08:25 +0200

    number animation and dummy data is added.

--- js/InfoGraphic/InfoGraphic.js
+++ js/InfoGraphic/InfoGraphic.js
@@ -1,3 +1,19 @@
 /**
  * Created by emre on 5.2.2015.
  */
+InfoGraphic = function() {
+    this.retrieveValues();
+};
+
+InfoGraphic.prototype.retrieveValues = function() {
+    $.getJSON( "data/infographic.json", function( data ) {
+        $("#plantValue")[0].innerHTML = data.plant;
+        $("#energyValue")[0].innerHTML = data.energy;
+        $("#meltingValue")[0].innerHTML = data.melting;
+        $("#radioactiveValue")[0].innerHTML = data.matter;
+        $("#diseaseValue")[0].innerHTML = data.disease;
+
+    });
+};
+
+new InfoGraphic();
